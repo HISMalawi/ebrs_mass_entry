ac4b51f50f53b09b20abdbd310a70761482d8833
Kenneth Kapundi <kennethkapundi1@gmail.com>
2019-04-17 15:09:30 +0200

    Script for reseting admin account

--- bin/reset_admin.rb
+++ bin/reset_admin.rb
@@ -3,11 +3,14 @@ admin_role = Role.where(name: "administrator").first
 
 user_roles = UserRole.where(user_id: u.id)
 if user_roles.length == 0
-	UserRole.create(role_id: admin_role.id, user_id: u.id))
+	UserRole.create(role_id: admin_role.id, user_id: u.id)
+	puts "Success!!"
 else
 
 	user_roles.each {|ur| 
 		ur.role_id = admin_role.id
 		ur.save
 	}
+
+	puts "Success!!"
 end
