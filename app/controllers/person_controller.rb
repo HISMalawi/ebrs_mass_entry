2f0622cdd7489a0536613e6599e9b917d8edae2c
Jacob Mziya <jacobmziya@gmail.com>
2020-05-20 10:28:04 +0200

    Denied administrators creation of births, redirecting to home on login instead.

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -53,6 +53,10 @@ class PersonController < ApplicationController
 
   def new
 
+    if @cur_user.role == 'Administrator'
+      redirect_to '/home' and return
+    end
+
     tag_id = LocationTag.where(name: 'District').last.id
     @districts = Location.joins(" join location_tag_map m ON m.location_id = location.location_id ")
     .where(" m.location_tag_id = #{tag_id} ").order("name")
