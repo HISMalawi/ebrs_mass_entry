1164fdfda5eb3685635dce27e2a5c8fa989719d2
Jacob Mziya <jacobmziya@gmail.com>
2020-11-05 00:34:56 +0200

    Added require csv for location tags.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :check_user, :except => ['logout', 'login']
-  before_filter :set_current_location, :except =>['set_current', 'districts', 'logout', 'login', 'tas', 'villages',"health_facilities"]
-  before_filter :current_location_to_readable, :except =>['set_current', 'districts', 'logout', 'login', 'tas', 'villages',"health_facilities"]
+  before_action :check_user, :except => ['logout', 'login']
+  before_action :set_current_location, :except =>['set_current', 'districts', 'logout', 'login', 'tas', 'villages',"health_facilities"]
+  before_action :current_location_to_readable, :except =>['set_current', 'districts', 'logout', 'login', 'tas', 'villages',"health_facilities"]
 
   def check_user
     if params[:active_tab].present?
--- db/load_location_tags.rb
+++ db/load_location_tags.rb
@@ -1,4 +1,7 @@
+require "csv"
+
 puts "Loading Location Tags"
+
 CSV.foreach("#{Rails.root}/app/assets/data/location_tags.csv", :headers => false) do |row|
  next if row[0].blank?
  location_tag = LocationTag.create!(name: row[0].squish, locked: 1)
